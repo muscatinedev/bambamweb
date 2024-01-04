6ac7d011d5b832853fa3764e329560c93782d17c
matteo scarpellini <muscatinedev@gmail.com>
2024-01-04 13:36:16 +0100

    contact

---
+++ website/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'website/home.html', {})
+
+def contact(request):
+
+    api_key='AIzaSyBpsimhniF4xRbQc_e63TEwtZMazWsy7xU'
+    context={"landmark":'Bergamo viale giulio cesare,26', "api_key":api_key}
+
+    return render(request, 'website/contact.html', context)
