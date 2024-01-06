fd308b99fe9b28d251dbd2ed2441f772a26ac60a
matteo scarpellini <muscatinedev@gmail.com>
2024-01-06 01:34:49 +0100

    plannermodel

--- planner/admin.py
+++ planner/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['dataTask','descrizione','spesa', 'completato' ,'dataCompletamento']
+
+    def get_ordering(self, request):
+        return ['-dataTask']  # sort case insensitive
+
+
+admin.site.register(Task, TaskAdmin)
--- planner/models.py
+++ planner/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    dataTask = models.DateField(null=True, blank=True)
+    descrizione = models.CharField(max_length=290)
+    spesa = models.FloatField(null=True, blank=True)
+    dataCompletamento = models.DateField(null=True, blank=True)
+    completato = models.BooleanField(default=False)
+
+
